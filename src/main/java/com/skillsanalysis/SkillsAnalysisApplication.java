c3131d3f0e2db82cf4a93a77c621f16d5b439bcf
Jonny Coddington <bottlerocketjonny@protonmail.com>
2022-10-31 16:20:44 +0000

    init commit

---
+++ src/main/java/com/skillsanalysis/SkillsAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.skillsanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillsanalysis/SkillsAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillsanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
