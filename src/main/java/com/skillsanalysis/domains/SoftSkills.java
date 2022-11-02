6beee5b9c2471c20c2634f0d772e0bef2db448b2
Jonny Coddington <bottlerocketjonny@protonmail.com>
2022-11-02 09:03:28 +0000

    Soft Skills entity added

---
+++ src/main/java/com/skillsanalysis/domains/SoftSkills.java
@@ -0,0 +1,38 @@
+package com.skillsanalysis.domains;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Entity
+public class SoftSkills {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String communication;
+	
+	private String problemSolving;
+	
+	private String leadership;
+	
+	private String punctuality;
+	
+	private String teamPlayer;
+
+}
